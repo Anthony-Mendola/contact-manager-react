e621d34b4b84d05b3b38442b6e7234283b5bc77c
Anthony <github email address>
2018-09-07 14:34:36 -0400

    removed constructor from state, not needed

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,9 +2,7 @@ import React, { Component } from "react";
 import Contact from "./Contact";
 
 class Contacts extends Component {
-  constructor() {
-    super();
-    this.state = {
+  state = {
       contacts: [
         {
           id: 1,
@@ -35,6 +33,7 @@ class Contacts extends Component {
       <div>
         {contacts.map(contact => (
           <Contact
+            key={contact.id}
             name={contact.name}
             email={contact.email}
             phone={contact.phone}
