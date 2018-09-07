cf4ea40d0b80874ce11e908bb9149e6d13c156ad
Anthony <github email address>
2018-09-07 14:40:34 -0400

    contact strings are now objects

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,28 +3,27 @@ import Contact from "./Contact";
 
 class Contacts extends Component {
   state = {
-      contacts: [
-        {
-          id: 1,
-          name: "Mark Cuban",
-          email: "markcuban@mavericks.com",
-          phone: "222-222-2222"
-        },
-        {
-          id: 2,
-          name: "Daymond John",
-          email: "daymond@thesharkgroup.com",
-          phone: "111-111-1111"
-        },
-        {
-          id: 3,
-          name: "Robert Herjavec",
-          email: "Robert@herjavecgroup.com .com",
-          phone: "333-333-3333"
-        }
-      ]
-    };
-  }
+    contacts: [
+      {
+        id: 1,
+        name: "Mark Cuban",
+        email: "markcuban@mavericks.com",
+        phone: "222-222-2222"
+      },
+      {
+        id: 2,
+        name: "Daymond John",
+        email: "daymond@thesharkgroup.com",
+        phone: "111-111-1111"
+      },
+      {
+        id: 3,
+        name: "Robert Herjavec",
+        email: "Robert@herjavecgroup.com .com",
+        phone: "333-333-3333"
+      }
+    ]
+  };
 
   render() {
     const { contacts } = this.state;
@@ -32,12 +31,7 @@ class Contacts extends Component {
     return (
       <div>
         {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            name={contact.name}
-            email={contact.email}
-            phone={contact.phone}
-          />
+          <Contact key={contact.id} contact={contact} />
         ))}
       </div>
     );
