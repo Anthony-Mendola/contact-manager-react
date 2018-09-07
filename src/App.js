2b5456a26846b7930ed082541c506b0219449f4e
Anthony <github email address>
2018-09-06 22:34:56 -0400

    added header component, added contact props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 import Contact from "./components/Contact";
+import Header from "./components/Header";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
-        <Contact />
+        <Header branding="Contact Manager" />
+        <Contact
+          name="Mark Cuban"
+          email="markcuban@mavericks.com"
+          phone="222-222-2222"
+        />
+        <Contact
+          name="Daymond John"
+          email="daymond@thesharkgroup.com"
+          phone="111-111-1111"
+        />
       </div>
     );
   }
