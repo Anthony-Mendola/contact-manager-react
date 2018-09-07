c3ffea7a6878a2da75515078531e54b6a5b880a5
Anthony <github email address>
2018-09-07 13:03:07 -0400

    added proptypes for validation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Header = props => {
   const { branding } = props;
@@ -11,6 +12,11 @@ const Header = props => {
 
 Header.defaultProps = {
   branding: "My App"
+};
+
+//validation check
+Header.PropTypes = {
+  branding: PropTypes.string.isRequired
 };
 
 export default Header;
