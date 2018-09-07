932e7d560a0090e8877a34bde3d2f91a775894d4
Anthony <github email address>
2018-09-07 14:20:52 -0400

    added Contacts compontent and state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Contact from "./components/Contact";
+import Contacts from "./components/Contacts";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -9,16 +9,9 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact
-          name="Mark Cuban"
-          email="markcuban@mavericks.com"
-          phone="222-222-2222"
-        />
-        <Contact
-          name="Daymond John"
-          email="daymond@thesharkgroup.com"
-          phone="111-111-1111"
-        />
+        <div className="container">
+          <Contacts />
+        </div>
       </div>
     );
   }
---
+++ src/components/Contacts.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import Contact from "./Contact";
+
+class Contacts extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: [
+        {
+          id: 1,
+          name: "Mark Cuban",
+          email: "markcuban@mavericks.com",
+          phone: "222-222-2222"
+        },
+        {
+          id: 2,
+          name: "Daymond John",
+          email: "daymond@thesharkgroup.com",
+          phone: "111-111-1111"
+        },
+        {
+          id: 3,
+          name: "Robert Herjavec",
+          email: "Robert@herjavecgroup.com .com",
+          phone: "333-333-3333"
+        }
+      ]
+    };
+  }
+
+  render() {
+    const { contacts } = this.state;
+
+    return (
+      <div>
+        {contacts.map(contact => (
+          <Contact
+            name={contact.name}
+            email={contact.email}
+            phone={contact.phone}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Contacts;
